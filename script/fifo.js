08dd8252898e1363ae845d1a31df44e92ffccd02
Steven Sierra <bizoru@gmail.com>
2016-04-14 00:53:54 -0500

    Arreglo de formato.

--- script/fifo.js
+++ script/fifo.js
@@ -8,51 +8,51 @@ var despachador = new Despachador();
 iniciar();
 
 
-function iniciar(){
+function iniciar() {
 
-    for(var i=0;i<10;i++){
-        colaListos.insertar(new Nodo("proceso "+i));
+    for (var i = 0; i < 10; i++) {
+        colaListos.insertar(new Nodo("proceso " + i));
     }
 
-    function reloj(){
-        setInterval(function(){
-
+    function reloj() {
+        setInterval(function () {
             tiempoGlobal += 1;
             actualizar();
 
-        },1000);
+        }, 1000);
     }
+
     reloj();
 
 
 }
 
-function actualizar(){
+function actualizar() {
 
     $("#tiempo-global").html();
     $("#tiempo-global").html(tiempoGlobal);
-    pintarCola(colaListos,".lista-listos");
-    pintarCola(colaBloqueados,".lista-bloqueados");
-    pintarCola(colaTerminados,".lista-terminados");
+    pintarCola(colaListos, ".lista-listos");
+    pintarCola(colaBloqueados, ".lista-bloqueados");
+    pintarCola(colaTerminados, ".lista-terminados");
 
 
 }
 
-function Despachador(){
+function Despachador() {
 
-    this.despachar = function(){
+    this.despachar = function () {
         // Manejo de la cola de bloqueados
 
         // Si la cpu esta libre asignar el primer proceso disponible de la cola de listos
-        if(!cpu.estaOcupado()){
-            if(!colaListos.estaVacia()){
+        if (!cpu.estaOcupado()) {
+            if (!colaListos.estaVacia()) {
                 cpu.ejecutar(colaListos.remover());
             }
-        }else{
-            if(cpu.nodo.proceso.tiempoEjecucionRestante == 0){
+        } else {
+            if (cpu.nodo.proceso.tiempoEjecucionRestante == 0) {
                 colaTerminados.insertar(cpu.liberar());
-            }else{
-                cpu.nodo.proceso.tiempoEjecucionRestante -=1;
+            } else {
+                cpu.nodo.proceso.tiempoEjecucionRestante -= 1;
             }
         }
         // Proceso disponible en la cola de bloqueados
@@ -62,88 +62,88 @@ function Despachador(){
 }
 
 
-function pintarCPU(selectorHtml){
+function pintarCPU(selectorHtml) {
 
     $(selectorHtml).html("");
-    if(cpu.estaOcupado()){
-        pintarNodoSimple(cpu.nodo,selectorHtml);
-    }else{
+    if (cpu.estaOcupado()) {
+        pintarNodoSimple(cpu.nodo, selectorHtml);
+    } else {
         cpuVacio(selectorHtml);
     }
 
 }
 
-function pintarCola(cola,selectorHtml){
+function pintarCola(cola, selectorHtml) {
 
     var colaAux = Object.create(cola);
 
-    if(cola.cambiado){
+    if (cola.cambiado) {
         $(selectorHtml).html("");
-        if(!cola.estaVacia()){
-            if(cola.traerRaiz()){
-                pintarNodo(cola.traerRaiz(),selectorHtml);
+        if (!cola.estaVacia()) {
+            if (cola.traerRaiz()) {
+                pintarNodo(cola.traerRaiz(), selectorHtml);
             }
-        }else{
+        } else {
             vacio(selectorHtml);
         }
     }
 }
 
-function pintarNodo(nodo,selectorHtml){
+function pintarNodo(nodo, selectorHtml) {
 
-    pintarNodoSimple(nodo,selectorHtml);
-    if(nodo.hijo){
-        pintarNodo(nodo.hijo,selectorHtml);
+    pintarNodoSimple(nodo, selectorHtml);
+    if (nodo.hijo) {
+        pintarNodo(nodo.hijo, selectorHtml);
     }
     return;
 }
 
-function pintarNodoSimple(nodo,selectorHtml){
+function pintarNodoSimple(nodo, selectorHtml) {
 
     $(selectorHtml).prepend(procesoHTML(nodo.proceso));
 }
 
-function procesoHTML(proceso){
-    var html = "<li><div>"+
-               "<p>"+proceso.nombre+"</p>"+
-               "<p>Tiempo rafaga: <b>"+proceso.rafaga+"</b></p>"+
-               "<p>Tiempo Restante Ejecucion "+proceso.tiempoEjecucionRestante+"</p>"+
-               "<p>Tiempo Bloqueo "+proceso.tiempoBloqueo+"</p>"+
-               "<p>Tiempo Llegada "+proceso.tiempoLlegada+"</p>"+
-               "</div></li>";
+function procesoHTML(proceso) {
+    var html = "<li><div>" +
+        "<p>" + proceso.nombre + "</p>" +
+        "<p>Tiempo rafaga: <b>" + proceso.rafaga + "</b></p>" +
+        "<p>Tiempo Restante Ejecucion " + proceso.tiempoEjecucionRestante + "</p>" +
+        "<p>Tiempo Bloqueo " + proceso.tiempoBloqueo + "</p>" +
+        "<p>Tiempo Llegada " + proceso.tiempoLlegada + "</p>" +
+        "</div></li>";
     return html;
 }
 
-function vacio(selectorHtml){
-    var html = "<li class='vacio'><div>"+
-        "<p> Cola Vacia</p>"+
+function vacio(selectorHtml) {
+    var html = "<li class='vacio'><div>" +
+        "<p> Cola Vacia</p>" +
         "</div></li>";
     $(selectorHtml).html(html);
 }
 
-function cpuVacio(selectorHtml){
-    var html = "<li class='vacio'><div>"+
-        "<p> CPU Disponible </p>"+
+function cpuVacio(selectorHtml) {
+    var html = "<li class='vacio'><div>" +
+        "<p> CPU Disponible </p>" +
         "</div></li>";
     $(selectorHtml).html(html);
 }
 
-function CPU(){
+function CPU() {
 
     this.nodo = undefined;
-    this.estaOcupado = function(){
-        if(this.nodo == undefined){
+    this.estaOcupado = function () {
+        if (this.nodo == undefined) {
             return false;
         }
         return true;
     };
 
-    this.ejecutar = function(nodo){
+    this.ejecutar = function (nodo) {
         this.nodo = nodo;
     }
 
     this.liberar = function () {
-        if(this.nodo){
+        if (this.nodo) {
             var n = this.nodo;
             n.hijo = undefined;
             this.nodo = undefined;
@@ -155,7 +155,7 @@ function CPU(){
 }
 
 
-function Nodo(nombre,hijo) {
+function Nodo(nombre, hijo) {
 
     this.hijo = hijo;
     this.proceso = new Proceso(nombre);
@@ -163,19 +163,19 @@ function Nodo(nombre,hijo) {
 }
 
 function Proceso(nombre) {
-     this.nombre = nombre;
-     this.rafaga = Math.floor((Math.random() * 10) + 2);
-     this.tiempoEjecucionRestante = this.rafaga;
-     this.tiempoBloqueo = 0;
-     this.tiempoLlegada = tiempoGlobal;
-     this.tiempoTerminado = undefined;
-     this.tiempoRetorno = undefined;
-     this.tiempoEspera = undefined;
-     this.tiempoComienzo = undefined;
+    this.nombre = nombre;
+    this.rafaga = Math.floor((Math.random() * 10) + 2);
+    this.tiempoEjecucionRestante = this.rafaga;
+    this.tiempoBloqueo = 0;
+    this.tiempoLlegada = tiempoGlobal;
+    this.tiempoTerminado = undefined;
+    this.tiempoRetorno = undefined;
+    this.tiempoEspera = undefined;
+    this.tiempoComienzo = undefined;
 
-     this.setTiempoLlegada = function(tiempo){
-         this.tiempoLlegada = tiempo;
-     }
+    this.setTiempoLlegada = function (tiempo) {
+        this.tiempoLlegada = tiempo;
+    }
 
 }
 
@@ -195,8 +195,8 @@ function Cola() {
         return true;
     };
 
-    this.traerRaiz =  function(){
-        if(this.nodoRaiz){
+    this.traerRaiz = function () {
+        if (this.nodoRaiz) {
             return this.nodoRaiz;
             this.cambiado = true;
         }
